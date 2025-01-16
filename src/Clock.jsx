cfcbd72cf643d551661cca1d3a80a6003edf2f15
mohammed faizan <faizan1999.in@gmail.com>
2025-01-17 02:50:27 +0530

    added 0's before single digits

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -9,7 +9,15 @@ export function Clock() {
   });
   return (
     <div className="clock">
-      {time.getHours()}:{time.getMinutes()}:{time.getSeconds()} PM
+      {time.getHours() < 10 ? `0${time.getHours()}` : `${time.getHours()}`}:
+      {time.getMinutes() < 10
+        ? `0${time.getMinutes()}`
+        : `${time.getMinutes()}`}
+      :
+      {time.getSeconds() < 10
+        ? `0${time.getSeconds()}`
+        : `${time.getSeconds()}`}{" "}
+      PM
     </div>
   );
 }
